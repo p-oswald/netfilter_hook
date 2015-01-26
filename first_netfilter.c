29965ae120862a8436df5376e2d978fa9c6d10a9
Patrick Oswald <patrickoswald@gmx.net>
2015-01-26 16:43:09 +0100

    tried to fix errors

--- first_netfilter.c
+++ first_netfilter.c
@@ -1,8 +1,7 @@
 //expansion of hello.c to include netfilter
 
-
-#define __KERNEL__
-#define __MODULE__
+//#define __KERNEL__
+//#undef __KERNEL__
 
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -10,10 +9,16 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 
+
+
 static struct nf_hook_ops nfho; //struct holding set of hook function options
 
-//unction to be called by hook
-unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff*))
+//function to be called by hook
+unsigned int hook_func_in(unsigned int hooknum, 
+												struct sk_buff *skb, 
+												const struct net_device *in, 
+												const struct net_device *out, 
+												int (*okfn)(struct sk_buff*))
 {
 	printk(KERN_INFO "packet dropped\n"); //log to /var/log/syslog
 	return NF_DROP;		//drops the packets
@@ -22,11 +27,11 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct 
 
 //called when module loaded via 'insmod'
 
-int init_module()
+int __init reg_my_hook()
 {
-	nfho.hook = hook_func;  //funtion to call when conditions below are met
-	nfho.hooknum = NF_IP_PRE_ROUTING;	//called right after packed recieved, first hook in Netfilter
-	nfho.pf = PR_INET;	//IPV4 packets
+	nfho.hook = hook_func_in;  //funtion to call when conditions below are met
+	nfho.hooknum = NF_INET_PRE_ROUTING;	//called right after packed recieved, first hook in Netfilter
+	nfho.pf = PF_INET;	//IPV4 packets
 	nfho.priority = NF_IP_PRI_FIRST;	//set to highest priorityover all other hook functions
 	nf_register_hook(&nfho);	//regiter hook
 	return 0;	//return 0 for no error
@@ -34,16 +39,16 @@ int init_module()
 
 //called when module is unloaded using 'rmmod'
 
-void cleanup_module()
+void __exit exi_my_hook()
 {
 	nf_unregister_hook(&nfho);	//cleanup - unregister hook
 }
 
+module_init(reg_my_hook);
+module_exit(exi_my_hook);
 
 /*
 static int __init config_init(void)
-
-
 {
 	printk(KERN_INFO "config_init executed successfully\n");
 	return 0;
